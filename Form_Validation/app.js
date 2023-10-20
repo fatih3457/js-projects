18186a912bc89884ecfced80ec646fe2bb496158
Fatih3457 <fatihozcan90@outlook.com>
2023-10-20 23:23:41 +0300

    third commit

--- Form_Validation/app.js
+++ Form_Validation/app.js
@@ -7,56 +7,66 @@ const password2 = document.getElementById('password2');
 form.addEventListener('submit', e => {
     e.preventDefault();
 
-    checkInputs();
+    validateInputs();
 });
 
-function checkInputs() {
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
     const usernameValue = username.value.trim();
     const emailValue = email.value.trim();
     const passwordValue = password.value.trim();
     const password2Value = password2.value.trim();
-}
 
-if (usernameValue === '') {
-    setErrorFor(username, 'Username cannot be blank');
-} else {
-    setSuccessFor(username);
-}
+    if(usernameValue === '') {
+        setError(username, 'Username is required');
+    } else {
+        setSuccess(username);
+    }
 
-if (emailValue === '') {
-    setErrorFor(email, "Email cannot ve blank");
-} else if (!isEmail(emailValue)) {
-    setErrorFor(email, 'Not a valid email');
-} else {
-    setSuccessFor(email);
-}
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
 
-if (passwordValue === '') {
-    setErrorFor(password, 'Password cannot be blank');
-} else {
-    setSuccessFor(password);
-}
+    if(passwordValue === '') {
+        setError(password, 'Password is required');
+    } else if (passwordValue.length < 8 ) {
+        setError(password, 'Password must be at least 8 character.')
+    } else {
+        setSuccess(password);
+    }
 
-if (password2Value === '') {
-    setErrorFor(password2, 'Password2 cannot be blank');
-} else if (passwordValue !== password2Value) {
-    setErrorFor(password2, 'Passwords does not match');
-} else {
-    setSuccessFor(password2);
-}
+    if(password2Value === '') {
+        setError(password2, 'Please confirm your password');
+    } else if (password2Value !== passwordValue) {
+        setError(password2, "Passwords doesn't match");
+    } else {
+        setSuccess(password2);
+    }
 
-function setErrorFor(input, message) {
-    const formControl = input.parentElement;
-    const small = formControl.querySelector ('small');
-    formControl.className = 'form-control-error';
-    small.innerText = message;
-}
-
-function setSuccessFor (input) {
-    const formControl = input.parentElement;
-    formControl.className = 'form-control-success';
-}
-
-
-
-
+};
